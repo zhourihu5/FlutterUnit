6a5c08d2d9ea4d304f41a75f747a9a3c0fcafe3b
toly <1981462002@qq.com>
2020-12-21 13:51:02 +0800

    更新windows

--- lib/storage/app_storage.dart
+++ lib/storage/app_storage.dart
@@ -6,9 +6,13 @@ import 'package:flutter/services.dart';
 import 'package:flutter_unit_mac/app/res/cons.dart';
 import 'package:flutter_unit_mac/app/res/sp.dart';
 import 'package:flutter_unit_mac/blocs/global/global_state.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart' as path;
+import 'package:sqflite_common/sqlite_api.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+import 'dao/db_setup/setup_db.dart';
 /// create by 张风捷特烈 on 2020-03-04
 /// contact me by email 1981462002@qq.com
 /// 说明:
@@ -47,21 +51,23 @@ class AppStorage {
 
   // 初始化数据库
   Future<Database> initDb() async {
-    var databasesPath = await getDatabasesPath();
-    var dbPath = path.join(databasesPath, "flutter.db");
-    var exists = await databaseExists(dbPath);
-    if (!exists) {
-      try {
-        await Directory(path.dirname(dbPath)).create(recursive: true);
-        print("========= assets ======拷贝完成====");
-      } catch (_) {}
-      ByteData data = await rootBundle.load(path.join("assets", "flutter.db"));
+    setupDatabase();
+    var databasesPath =  await getApplicationSupportDirectory();
+    var dbPath = path.join(databasesPath.path, "flutter.db");
+
+    var file = File(dbPath);
+    if (!file.existsSync()) {
+      await Directory(path.dirname(dbPath)).create(recursive: true);
+      ByteData data = await rootBundle.load("assets/flutter.db");
       List<int> bytes =
       data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
       await File(dbPath).writeAsBytes(bytes, flush: true);
+      print("========= assets ======拷贝完成==${file.path}==");
     } else {
-      print("========= 数据库 ======已存在====");
+      print("========= 数据库 ======已存在==${file.path}==");
     }
-    return await openDatabase(dbPath, readOnly: false);
+
+    var databaseFactory = databaseFactoryFfi;
+    return await databaseFactory.openDatabase(dbPath);
   }
 }
--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -4,28 +4,27 @@
 
 #include "flutter_window.h"
 #include "run_loop.h"
-#include "window_configuration.h"
+#include "utils.h"
 
-int APIENTRY wWinMain(_In_ HINSTANCE instance,
-                      _In_opt_ HINSTANCE prev,
-                      _In_ wchar_t* command_line,
-                      _In_ int show_command) {
+int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
+                      _In_ wchar_t *command_line, _In_ int show_command) {
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
-    ::AllocConsole();
+    CreateAndAttachConsole();
   }
 
-  // Initialize COM, so that it is available for use in the library and/or plugins.
+  // Initialize COM, so that it is available for use in the library and/or
+  // plugins.
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
   RunLoop run_loop;
 
   flutter::DartProject project(L"data");
   FlutterWindow window(&run_loop, project);
-  Win32Window::Point origin(kFlutterWindowOriginX, kFlutterWindowOriginY);
-  Win32Window::Size size(kFlutterWindowWidth, kFlutterWindowHeight);
-  if (!window.CreateAndShow(kFlutterWindowTitle, origin, size)) {
+  Win32Window::Point origin(200, 100);
+  Win32Window::Size size(800, 600);
+  if (!window.CreateAndShow(L"flutter_db", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
